2df2f6c9f1e4a3fb0c8670e86277b6ad953d3eb3
Albert DeFusco <defusco@pitt.edu>
2015-06-18 16:01:18 -0400

    attempting to do fftw

---
+++ var/spack/packages/fftw/package.py
@@ -0,0 +1,45 @@
+# FIXME:
+# This is a template package file for Spack.  We've conveniently
+# put "FIXME" labels next to all the things you'll want to change.
+#
+# Once you've edited all the FIXME's, delete this whole message,
+# save this file, and test out your package like this:
+#
+#     spack install fftw
+#
+# You can always get back here to change things with:
+#
+#     spack edit fftw
+#
+# See the spack documentation for more information on building
+# packages.
+#
+from spack import *
+
+class Fftw(Package):
+    """FIXME: put a proper description of your package here."""
+    # FIXME: add a proper url for your package's homepage here.
+    homepage = "http://www.example.com"
+    url      = "http://www.fftw.org/fftw-3.3.4.tar.gz"
+
+    version('3.3.4', '2edab8c06b24feeb3b82bbb3ebf3e7b3')
+
+    # FIXME: Add dependencies if this package requires them.
+    # depends_on("foo")
+
+    depends_on('mpi')
+
+    def install(self, spec, prefix):
+        # FIXME: Modify the configure line to suit your build system here.
+        config_args = ["--prefix=%s" % prefix, "CC=mpicc", "CXX=mpicxx", "FC=mpif90"]
+
+	if spec.satisfies("=haswell"):
+	  config_args.extend( ["--enable-avx",
+	                       "--enable-fma",
+			       "--enable-openmp",
+			       "--enable-mpi"] )
+
+
+        configure(*config_args) 
+        make()
+        make("install")
--- var/spack/packages/openmpi/package.py
+++ var/spack/packages/openmpi/package.py
@@ -1,4 +1,5 @@
 from spack import *
+import os
 
 class Openmpi(Package):
     """Open MPI is a project combining technologies and resources from
@@ -13,6 +14,8 @@ class Openmpi(Package):
 
     version('1.8.2', 'ab538ed8e328079d566fc797792e016e',
             url='http://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.2.tar.gz')
+    version('1.8.6', 'f4d747d39ac2b8cdd8be6d847cd9228d',
+	url='http://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.6.tar.gz')
     version('1.6.5', '03aed2a4aa4d0b27196962a2a65fc475',
             url = "http://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.5.tar.bz2")
 
@@ -35,7 +38,42 @@ class Openmpi(Package):
         # compiler configuration.
         if not self.compiler.f77 and not self.compiler.fc:
             config_args.append("--enable-mpi-fortran=no")
+	
+	if spec.architecture == "haswell":
+	  config_args.extend([ "--with-slurm",
+	                       "--enable-mpi-fortran",
+			       "--with-openib"
+			       ])
+
 
         configure(*config_args)
         make()
         make("install")
+
+        self.filter_compilers()
+
+    def filter_compilers(self):
+        """Run after install to make the MPI compilers use the
+           compilers that Spack built the package with.
+           If this isn't done, they'll have CC, CXX, F77, and FC set
+           to Spack's generic cc, c++, f77, and f90.  We want them to
+           be bound to whatever compiler they were built with.
+        """
+        bin = self.prefix.bin
+        mpicc  = os.path.join(bin, 'mpicc')
+        mpicxx = os.path.join(bin, 'mpicxx')
+        mpif77 = os.path.join(bin, 'mpif77')
+        mpif90 = os.path.join(bin, 'mpif90')
+
+        kwargs = { 'ignore_absent' : True, 'backup' : False, 'string' : True }
+        filter_file('CC="cc"',   'CC="%s"'  % self.compiler.cc,  mpicc,  **kwargs)
+        filter_file('CXX="c++"', 'CXX="%s"' % self.compiler.cxx, mpicxx, **kwargs)
+        filter_file('F77="f77"', 'F77="%s"' % self.compiler.f77, mpif77, **kwargs)
+        filter_file('FC="f90"',  'FC="%s"'  % self.compiler.fc,  mpif90, **kwargs)
+
+    def setup_dependent_environment(self, module, spec, dep_spec):
+        """For dependencies, make mpicc's use spack wrapper."""
+        os.environ['MPICH_CC']  = 'cc'
+        os.environ['MPICH_CXX'] = 'c++'
+        os.environ['MPICH_F77'] = 'f77'
+        os.environ['MPICH_F90'] = 'f90'
